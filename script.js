3f28930dcce32ccacdaa8c7605a221b124f6448c
Victor Mello <victorsmello93@gmail.com>
2021-02-06 20:13:06 -0300

    Ajustes no css para incluir mais variáveis de cores Implementada a lógica de salvar o estado do seletor de temas no Local Storage

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 window.onload = () => {
-    const html = document.querySelector('html');
-    const checkbox = document.getElementById('checkbox');
-  
-    checkbox.addEventListener('change', () => {
-      html.classList.toggle('dark-mode')
-    });
+  const html = document.querySelector('html');
+  const checkbox = document.getElementById('checkbox');
+
+  if (localStorage.getItem('background') === 'true') {
+    html.classList.toggle('dark-mode')
+    checkbox.checked = true
   }
+
+  checkbox.addEventListener('change', () => {
+    html.classList.toggle('dark-mode')
+    localStorage.setItem('background', checkbox.checked)
+  });
+}
